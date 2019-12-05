f6b9d9a098397da194e373a5b3a97aa15c7d5c96
minh <hoangminh160997@gmail.com>
2019-12-05 06:43:33 -0800

    add modal

--- models/Test.js
+++ models/Test.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 
 const testSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    questionSet: { type: mongoose.Types.ObjectId, required: true },
+    questionSet: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "questionset"
+    },
     duration: { type: Number },
-    start: { type: Date, required: true }
+    start: { type: Date, required: true },
+    owner: { type: mongoose.Types.ObjectId, required: true, ref: "user" }
 });
 
 const Record = mongoose.model("test", testSchema);
