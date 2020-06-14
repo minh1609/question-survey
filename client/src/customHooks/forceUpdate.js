0594913c01b2bfcea5431056e2cc3ceff99824c4
Minh Nguyen <hoangminh160997@gmail.com>
2020-06-14 14:33:13 -0700

    remove

--- client/src/customHooks/forceUpdate.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-//create your forceUpdate hook
-function useForceUpdate() {
-    const [value, setValue] = useState(true); //boolean state
-    return () => setValue(!value); // toggle the state to force render
-}
-export default useForceUpdate;
